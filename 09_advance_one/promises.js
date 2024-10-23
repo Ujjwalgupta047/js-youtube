a51cc9844dff5c949ae89f5c3126c02ee716555f
Ujjwalgupta047 <146421055+Ujjwalgupta047@users.noreply.github.com>
2024-10-23 11:12:15 +0000

    change

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -8,38 +8,87 @@
 //      console.log('async 1 resolved')
 // })
 
-const promiseTwo=new Promise((resolve,reject)=>{
+// const promiseTwo=new Promise((resolve,reject)=>{
        
-    setTimeout(()=>{
-         resolve({name:"ujjwal gupta",email:"ujjwal@gmail.com"})
-    },1000)
+//     setTimeout(()=>{
+//          resolve({name:"ujjwal gupta",email:"ujjwal@gmail.com"})
+//     },1000)
+// })
+
+// promiseTwo.then((user)=>{
+//      console.log(user)
+// })
+
+// const promiseThree=new Promise((resolve,reject)=>{
+//        let error=true;
+
+//        if(!error){
+//             setTimeout(()=>{
+//                 resolve({username:"ujjwal gupta",password:"547"})
+//             },1000)
+//        } 
+//         else{
+//             reject('Error : somenthing went wrong')
+//         }
+// })
+
+// promiseThree
+// .then((user)=>{
+//      console.log(user)
+//      return user.username
+// })
+// .then((username)=>{
+//      console.log(username)
+// })
+// .catch((e)=>{
+//      console.log(e)
+// })
+
+const promiseFour=new Promise((resolve,reject)=>{
+     let error=false;
+
+     if(!error){
+          setTimeout(()=>{
+              resolve({username:"ujjwal gupta 4",password:"5474"})
+          },1000)
+     } 
+      else{
+          reject('Error : somenthing went wrong')
+      }
 })
 
-promiseTwo.then((user)=>{
-     console.log(user)
-})
+async function consumePromises(){
+     try{
+     const  response =await promiseFour
+     console.log(response)
+    }
+    catch(e){
+             console.log(e)
+    }
+}
 
-const promiseThree=new Promise((resolve,reject)=>{
-       let error=true;
+consumePromises()
 
-       if(!error){
-            setTimeout(()=>{
-                resolve({username:"ujjwal gupta",password:"547"})
-            },1000)
-       } 
-        else{
-            reject('Error : somenthing went wrong')
-        }
-})
+// async function getAllUser(){
+//     try{
+//        const response= await fetch('https://jsonplaceholder.typicode.com/users')
+//        const data= await response.json()
+//        console.log(data)
+//     }
+//     catch(e){
+//          console.log('E :',e); 
+//     }
+// }
 
-promiseThree
-.then((user)=>{
-     console.log(user)
-     return user.username
+// getAllUser()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
+    return response.json()
 })
-.then((username)=>{
-     console.log(username)
+.then((data)=>{
+     console.log(data)
 })
 .catch((e)=>{
-     console.log(e)
+    console.log("E :",e)
 })
---
+++ 10_classes_and_oop/notes.md
@@ -0,0 +1,21 @@
+# javascript and classes
+
+## oop
+
+## object
+ -collection of properties and member function
+ -toLowerCase
+
+ ## why use oop
+ object literals
+
+ -constructor functions
+ -prototype
+ -classes
+ -instancesn (new,this)
+
+ ## four pillars of oop
+  abstraction
+  incapsulation
+  inheritance
+  polymorphism
